c02dbcd1a208ade0b2def630df6772e8d73ddab8
denes.solti <sodnaatx@gmail.com>
2024-01-23 05:19:15 +0100

    perf test II

--- PERF/JsonParser.cs
+++ PERF/JsonParser.cs
@@ -4,6 +4,8 @@
 * Author: Denes Solti                                                           *
 ********************************************************************************/
 using System;
+using System.Collections.Generic;
+using System.Threading;
 
 using BenchmarkDotNet.Attributes;
 
@@ -12,23 +14,44 @@ namespace Solti.Utils.Json.Perf
     internal sealed class DummyContext : IJsonReaderContext
     {
         public void CommentParsed(ReadOnlySpan<char> value) => throw new NotImplementedException();
-        public object CreateRawObject(ObjectKind objectKind) => throw new NotImplementedException();
+        public object CreateRawObject(ObjectKind objectKind) => new List<object?>();
         public void PopState() => throw new NotImplementedException();
         public bool PushState(ReadOnlySpan<char> property, StringComparison comparison) => throw new NotImplementedException();
-        public void SetValue(object obj, object? value) => throw new NotImplementedException();
-        public void SetValue(object obj, ReadOnlySpan<char> value) => throw new NotImplementedException();
+        public void SetValue(object obj, object? value) => ((List<object?>) obj).Add(value);
+        public void SetValue(object obj, ReadOnlySpan<char> value) => ((List<object?>) obj).Add(new string(value));
     }
 
     [MemoryDiagnoser]
-    public class StringParsing
+    public class JsonParser
     {
         [
             Params
             (
+                //
+                // string
+                //
+
                 "\"cica\"",
                 "\"cica\\r\\n\"",
                 "\"Let's smile \\uD83D\\uDE01\"",
-                "\"Loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong\""
+                "\"Loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong\"",
+                
+                //
+                // Number
+                //
+
+                "100",
+                "-100",
+                "100.0",
+                "-100.0",
+
+                //
+                // List
+                //
+
+                "[]",
+                "[true]",
+                "[0, true, false, null, \"cica\"]"
             )
         ]
         public string Input { get; set; } = null!;
@@ -48,7 +71,7 @@ namespace Solti.Utils.Json.Perf
         public void Parse()
         {
             Content.Reset();
-            _ = Reader.ParseString();
+            _ = Reader.Parse(CancellationToken.None);
         }
     }
 }
