9354ae23777d2179a3f0c0d877a9b2b302a7e45e
denes.solti <sodnaatx@gmail.com>
2024-02-17 08:49:25 +0100

    pretty print inputs in JsonReaderTests

--- PERF/JsonReader.cs
+++ PERF/JsonReader.cs
@@ -3,7 +3,6 @@
 *                                                                               *
 * Author: Denes Solti                                                           *
 ********************************************************************************/
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
@@ -16,7 +15,32 @@ namespace Solti.Utils.Json.Perf
     [MemoryDiagnoser]
     public class JsonReaderTests
     {
-        public static IEnumerable<Func<TextReader>> Params
+        public abstract class ContentFactory
+        {
+            protected ContentFactory(string name) => Name = name;
+            public string Name { get; } 
+            public abstract TextReader CreateReader();
+            public override string ToString() => Name;
+        }
+
+        public class StringContentFactory(string str) : ContentFactory(str)
+        {
+            public override TextReader CreateReader() => new StringReader(str);
+        }
+
+        public class FileContentFactory(string fileName) : ContentFactory(fileName)
+        {
+            public override TextReader CreateReader() => new StreamReader
+            (
+                Path.Combine
+                (
+                    Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!,
+                    fileName
+                )
+            );
+        }
+
+        public static IEnumerable<ContentFactory> Params
         {
             get
             {
@@ -24,55 +48,46 @@ namespace Solti.Utils.Json.Perf
                 // String
                 //
 
-                yield return static () => new StringReader("\"cica\"");
-                yield return static () => new StringReader("\"cica\\r\\n\"");
-                yield return static () => new StringReader("\"Let's smile \\uD83D\\uDE01\"");
+                yield return new StringContentFactory("\"cica\"");
+                yield return new StringContentFactory("\"cica\\r\\n\"");
+                yield return new StringContentFactory("\"Let's smile \\uD83D\\uDE01\"");
 
                 //
                 // Number
                 //
 
-                yield return static () => new StringReader("100");
-                yield return static () => new StringReader("-100");
-                yield return static () => new StringReader("100.0");
-                yield return static () => new StringReader("-100.0");
+                yield return new StringContentFactory("100");
+                yield return new StringContentFactory("-100");
+                yield return new StringContentFactory("100.0");
+                yield return new StringContentFactory("-100.0");
 
                 //
                 // List
                 //
 
-                yield return static () => new StringReader("[]");
-                yield return static () => new StringReader("[true]");
-                yield return static () => new StringReader("[0, true, false, null, \"cica\"]");
+                yield return new StringContentFactory("[]");
+                yield return new StringContentFactory("[true]");
+                yield return new StringContentFactory("[0, true, false, null, \"cica\"]");
 
                 //
                 // Object
                 //
 
-                yield return static () => new StringReader("{}");
-                yield return static () => new StringReader("{\"cica\": 1986}");
-                yield return static () => new StringReader("{\"a\": 0, \"b\": true, \"c\": false, \"d\": null, \"e\": \"cica\"}");
+                yield return new StringContentFactory("{}");
+                yield return new StringContentFactory("{\"cica\": 1986}");
+                yield return new StringContentFactory("{\"a\": 0, \"b\": true, \"c\": false, \"d\": null, \"e\": \"cica\"}");
 
                 //
                 // Mixed, large
                 //
 
-                foreach (string file in new string[] { "large1.json", "large2.json" })
-                {
-                    yield return () => new StreamReader
-                    (
-                        Path.Combine
-                        (
-                            Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!,
-                            file
-                        )
-                    );
-                }
+                yield return new FileContentFactory("large1.json");
+                yield return new FileContentFactory("large2.json");
             }
         }
 
         [ParamsSource(nameof(Params))]
-        public Func<TextReader> Input { get; set; } = null!;
+        public ContentFactory Input { get; set; } = null!;
 
         public JsonReader Reader { get; set; } = null!;
 
@@ -82,7 +97,7 @@ namespace Solti.Utils.Json.Perf
         [Benchmark]
         public void Parse()
         {
-            using TextReader content = Input();
+            using TextReader content = Input.CreateReader();
 
             _ = Reader.Parse(content, DeserializationContext.Untyped, CancellationToken.None);
         }
