d31e773a5724fbb99874b95fb370f3338cbd104c
denes.solti <sodnaatx@gmail.com>
2024-02-18 06:43:19 +0100

    introduce MemoryExtensions.GetXxHashCode() method

--- SRC/Private/MemoryExtensions.cs
+++ SRC/Private/MemoryExtensions.cs
@@ -4,6 +4,7 @@
 * Author: Denes Solti                                                           *
 ********************************************************************************/
 using System;
+using System.IO.Hashing;
 using System.Runtime.CompilerServices;
 
 namespace Solti.Utils.Json.Internals
@@ -26,7 +27,18 @@ namespace Solti.Utils.Json.Internals
 #endif
         }
 
+        private static readonly int FSeed = new Random().Next();
+
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static string AsString(this Span<char> self) => ((ReadOnlySpan<char>) self).AsString();
+        unsafe public static int GetXxHashCode(this ReadOnlySpan<char> self)
+        {
+            fixed (void* ptr = self)
+            {
+                return (int) XxHash32.HashToUInt32(new ReadOnlySpan<byte>(ptr, self.Length * sizeof(char)), FSeed);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string AsString(this Span<char> self) => AsString((ReadOnlySpan<char>) self);
     }
 }
---
+++ TEST/MemoryExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿/********************************************************************************
+* MemoryExtensionsTests.cs                                                      *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+
+using NUnit.Framework;
+
+namespace Solti.Utils.Json.Internals.Tests
+{
+    [TestFixture]
+    public class MemoryExtensionsTests
+    {
+        [Test]
+        public void AsString_ShouldStringify([Values("", "1", "1986", "cica", "😁")] string str) =>
+            Assert.That(str.AsSpan().AsString(), Is.EqualTo(str));
+
+        [Test]
+        public void GetXxHashCode_ShouldHash([Values("", "1", "1986", "cica", "😁")] string str)
+        {
+            Assert.AreEqual(str.AsSpan().GetXxHashCode(), str.Substring(0).AsSpan().GetXxHashCode());
+            Assert.AreNotEqual(str.AsSpan().GetXxHashCode(), (str + "_").AsSpan().GetXxHashCode());
+        }
+    }
+}
