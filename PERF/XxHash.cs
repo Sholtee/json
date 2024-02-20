9041a29e7ef67f8bdefc7dd55d154858554a6ed3
denes.solti <sodnaatx@gmail.com>
2024-02-20 05:17:24 +0100

    XxHash performance tests

---
+++ PERF/XxHash.cs
@@ -0,0 +1,23 @@
+﻿/********************************************************************************
+* XxHash.cs                                                                     *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+
+using BenchmarkDotNet.Attributes;
+
+namespace Solti.Utils.Json.Perf
+{
+    using Internals;
+
+    [MemoryDiagnoser]
+    public class XxHashTests
+    {
+        [Params("a", "ab", "abcd", "abcdefgh", "abcdefghijklmnopqrstuvwz")]
+        public string Input { get; set; } = null!;
+
+        [Benchmark()]
+        public int GetXxHashCode() => Input.AsSpan().GetXxHashCode();
+    }
+}
--- PERF/index.md
+++ PERF/index.md
@@ -3,3 +3,4 @@
 - [JsonReader](https://sholtee.github.io/json/perf/Solti.Utils.Json.Perf.JsonReaderTests-report-github.html )
 - [JsonWriter](https://sholtee.github.io/json/perf/Solti.Utils.Json.Perf.JsonWriterTests-report-github.html )
 - [StringKeyedDictionary](https://sholtee.github.io/json/perf/Solti.Utils.Json.Perf.StringKeyedDictionaryTests-report-github.html )
+- [XxHash](https://sholtee.github.io/json/perf/Solti.Utils.Json.Perf.XxHashTests-report-github.html )
