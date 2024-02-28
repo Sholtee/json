32d54522b3f68e53babd307975d46b5af61fcf71
denes.solti <sodnaatx@gmail.com>
2024-02-28 18:51:14 +0100

    rename CommentParserDelegate -> ParseCommentDelegate

--- SRC/Public/DeserializationContext.cs
+++ SRC/Public/DeserializationContext.cs
@@ -21,7 +21,7 @@ namespace Solti.Utils.Json
         /// </summary>
         public delegate bool GetListItemContextDelegate(int index, out DeserializationContext context);
 
-        public delegate void CommentParserDelegate(ReadOnlySpan<char> value);
+        public delegate void ParseCommentDelegate(ReadOnlySpan<char> value);
 
         public delegate object RawObjectFavtoryDelegate();
 
@@ -50,7 +50,7 @@ namespace Solti.Utils.Json
         /// Method to be called when a comment section has been parsed successfully.
         /// </summary>
         /// <remarks>This method gets invoked only when the <see cref="JsonParserFlags.AllowComments"/> flag is set.</remarks>
-        public CommentParserDelegate? CommentParser { get; init; }
+        public ParseCommentDelegate? CommentParser { get; init; }
 
         /// <summary>
         /// Creates a rawobject instance, if supported.
