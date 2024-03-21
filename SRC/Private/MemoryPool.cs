f2d1727025e6f29fcdc8b2d1c89513b4a0abbcd7
denes.solti <sodnaatx@gmail.com>
2024-03-21 07:07:36 +0100

    threshold over which we won't retain the buffer

--- SRC/Private/MemoryPool.cs
+++ SRC/Private/MemoryPool.cs
@@ -4,11 +4,14 @@
 * Author: Denes Solti                                                           *
 ********************************************************************************/
 using System.Collections.Concurrent;
+using System.Runtime.CompilerServices;
 
 namespace Solti.Utils.Json.Internals
 {
     internal static class MemoryPool<T>
     {
+        private static readonly int FSizeOfT = Unsafe.SizeOf<T>();
+
         //
         // Do not use ConcurrentBag here as it significantly slower than ConcurrentStack
         //
@@ -29,6 +32,14 @@ namespace Solti.Utils.Json.Internals
                 : [];
         }
 
-        public static void Return(T[] buffer) => FPool.Push(buffer);
+        public static void Return(T[] buffer)
+        {
+            //
+            // Do not retain buffers bigger than 1MB
+            //
+
+            if (buffer.Length * FSizeOfT < 1024 * 1024)
+                FPool.Push(buffer);
+        }
     }
 }
