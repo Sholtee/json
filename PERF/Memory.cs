12dfcad483b8fed58df59c79be3445a2c245b730
denes.solti <sodnaatx@gmail.com>
2024-06-11 19:37:23 +0200

    index.md up, remove obsolete perf tests

--- PERF/Memory.cs
+++ PERF/Memory.cs
@@ -5,98 +5,12 @@
 ********************************************************************************/
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 using BenchmarkDotNet.Attributes;
 
 namespace Solti.Utils.Json.Perf
 {
     using Internals;
-
-    [MemoryDiagnoser]
-    public class BuiltInsTests
-    {
-        private static readonly char[]
-            Input = Enumerable.Repeat(' ', 1024 * 5).ToArray(),
-            Dest = new char[Input.Length];
-
-        [Benchmark]
-        public void SingleCopy()
-        {
-            Span<char>
-                src = Input.AsSpan(),
-                dst = Dest.AsSpan();
-
-            for (int i = 0; i < Input.Length; i++)
-            {
-                dst[i] = src[i];
-            }
-        }
-
-        [Benchmark]
-        public void SpanCopy()
-        {
-            Span<char>
-                src = Input.AsSpan(),
-                dst = Dest.AsSpan();
-            src.CopyTo(dst);
-        }
-
-        [Benchmark]
-        public void Enumerate()
-        {
-            Span<char> src = Input.AsSpan();
-            foreach (char c in src)
-            {
-                _ = c;
-            }
-        }
-
-        [Benchmark]
-        public void Iterate()
-        {
-            Span<char> src = Input.AsSpan();
-            for (int i = 0; i < src.Length; i++)
-            {
-                _ = src[i];
-            }
-        }
-
-        private static readonly char[] FControlChars = GetControlChars().ToArray();
-
-        private static IEnumerable<char> GetControlChars()
-        {
-            for (int ctr = 0x00; ctr <= 0xFFFF; ctr++)
-            {
-                char c = (char)ctr;
-                if (char.IsControl(c))
-                    yield return c;
-            }
-        }
-
-        [Benchmark]
-        public int FindControl() => Input.AsSpan().IndexOfAny(FControlChars);
-
-        [Benchmark]
-        public void FindControlRange()
-        {
-            _ = Input.AsSpan().IndexOfAnyInRange((char) 0000, (char) 001F);
-            _ = Input.AsSpan().IndexOfAnyInRange((char) 007F, (char) 009F);
-        }
-
-        [Benchmark]
-        public int FindControlShort() => Input.AsSpan().IndexOfAny("\r\n\t");
-
-        [Benchmark]
-        public void FindControlUsingIsControl()
-        {
-            Span<char> src = Input.AsSpan();
-            for (int i = 0; i < src.Length; i++)
-            {
-                _ = char.IsControl(src[i]);
-            }
-        }
-    }
 
     [MemoryDiagnoser]
     public class HashTests
--- PERF/index.md
+++ PERF/index.md
@@ -1,6 +1,7 @@
 # Solti.Utils.Json Benchmark Results
 
+- [Hash](https://sholtee.github.io/json/perf/Solti.Utils.Json.Perf.HashTests-report-github.html )
+- [IndexOfAnyExcept](https://sholtee.github.io/json/perf/Solti.Utils.Json.Perf.IndexOfAnyExceptTests-report-github.html )
 - [JsonParser](https://sholtee.github.io/json/perf/Solti.Utils.Json.Perf.JsonParserTests-report-github.html )
 - [JsonWriter](https://sholtee.github.io/json/perf/Solti.Utils.Json.Perf.JsonWriterTests-report-github.html )
 - [StringKeyedDictionary](https://sholtee.github.io/json/perf/Solti.Utils.Json.Perf.StringKeyedDictionaryTests-report-github.html )
-- [XxHash](https://sholtee.github.io/json/perf/Solti.Utils.Json.Perf.XxHashTests-report-github.html )
